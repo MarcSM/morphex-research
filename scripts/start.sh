14e247aafa7d0f33719e6a6edfa18ed4f26fb9b2
MarcSM <discotazo@gmail.com>
2019-09-01 20:25:18 +0200

    Master Thesis Submission

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,22 +1,9 @@
 #!/usr/bin/env bash
 
-# Running the voila server
-until cd /notebooks/notebooks/examples/voila-notebooks/
-do
-    echo "Waiting for the volumes to be mounted"
-    sleep 4 # Wait 4 seconds
-done
-
-echo "Running the voila application"
-voila --port=4444 bqplot.ipynb & # Voila application
-
-echo "Voila application up and running"
-
-
-echo "Starting the PulseAudio daemon"
-pulseaudio --log-level=4 --log-target=stderr -v &
+# echo "Starting the PulseAudio daemon"
+# pulseaudio --log-level=4 --log-target=stderr -v &
 
 # Running the Jupyter Notebooks server
 echo "Running Jupyter Notebooks"
 cd /notebooks/
-jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --no-browser
+jupyter notebook --ip=0.0.0.0 --port=4444 --allow-root --no-browser
